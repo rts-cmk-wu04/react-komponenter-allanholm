a361e19f9a0ffe23d2c0f41a313de233bdb8da31
Allan Holm Søndergaard <6605392@rts-365.dk>
2021-01-13 13:38:15 +0100

    cards with pix + cleanup

--- src/pages/Mid.js
+++ src/pages/Mid.js
@@ -2,9 +2,12 @@ import { useEffect, useState } from "react";
 import "./Mid.css";
 import BigArticleMid from "../components/BigArticleMid";
 import LatestNews from "../components/LatestNews";
+import CardWithPic from "../components/CardWithPic";
 
 function Mid() {
   var [articles, setArticles] = useState([]);
+  var [cards, setCards] = useState([]);
+
   
   useEffect(function() {
     fetch("./database.json")
@@ -13,15 +16,23 @@ function Mid() {
     })
     .then(function(data) {
       setArticles(data.bigArticlesMid);
+      setCards(data.cardsWPic);
     })
   }, []);
 
   return (
     <section className="middleSection">
       <section className="bigArticles">
+        {/*  artikel kald */}
         {articles.map(function (article) {
           return <BigArticleMid key={ article.heading } article={ article } />;
         })}
+        { /*cards kald.*/}
+        <div className="cardWithPic">
+          {cards.map(function (card) {
+            return <CardWithPic key={ card.title } card={ card } />;
+          })}
+        </div>
       </section>
       <LatestNews />
     </section>
